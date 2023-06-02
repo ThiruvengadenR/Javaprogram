56380d3c941d389659c47bd8b41e5832e9470446
ThiruvengadenR <thiruvengadenr2002@gmail.com>
2023-06-02 10:26:35 +0530

    first program

---
+++ Sample.java
@@ -0,0 +1,12 @@
+public class Sample // class name should be same as file name 
+{
+public static void main (String args[])
+{
+System.out.println("My third section");
+long no1 = 9865019444l;
+int no2 = 20;
+int no3 = no2+ (int)no1;// type casting 
+System.out.println(no3);
+}
+
+}
