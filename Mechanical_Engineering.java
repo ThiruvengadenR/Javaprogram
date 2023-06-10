83ea5bea26d693335bd3e4d6e2fbea2a4f8d6d33
ThiruvengadenR <thiruvengadenr2002@gmail.com>
2023-06-10 12:13:19 +0530

    method overriding child class

---
+++ Mechanical_Engineering.java
@@ -0,0 +1,18 @@
+class Mechanical_Engineering extends Engineering 
+{
+    public static void main (String [] args )
+    {
+        Mechanical_Engineering mechObj = new Mechanical_Engineering();
+        mechObj.attendance();
+        mechObj.project();
+        
+    }
+
+void project()
+    {
+        System.out.println(" im studying under  " + university_name );
+        System.out.println(" your projects must comes under design or manufacturing sector  ");
+       
+    }
+}
+
