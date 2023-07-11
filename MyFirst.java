1418763482ba3e7fb2a44ff19340887107788792
ThiruvengadenR <thiruvengadenr2002@gmail.com>
2023-07-11 07:20:32 +0530

    july 10 mcq dicussion shows flow of block static block , static method, constructorand instances block

---
+++ MyFirst.java
@@ -0,0 +1,29 @@
+package whileloop;
+
+public class MyFirst {  
+    public static void main(String[] args) { 
+        System.out.println(n);//we refer line number 9
+       MyFirst obj = new MyFirst(n);  
+}  
+static int a = 10;  
+static double n;  
+static int b = 5;  
+int c;  
+public MyFirst(double m) {
+    System.out.println(m);
+     System.out.println(a + ", " + b + ", " + c + ", " + n + ", " + m);  
+
+ }  
+// Instance Block  
+{  
+	System.out.println("my instance block is working");
+   b = 30;  
+   n = 20;  
+}   
+// Static Block  
+static   
+{  
+	System.out.println("my static block is working");
+        b = 60;  
+   }   
+}
